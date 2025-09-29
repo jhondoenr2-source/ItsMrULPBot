df25362d4b383e1f8d14efd4a4bd3e07f068bcbb
igameTEN <jhondoenr2@gmail.com>
2025-09-29 13:16:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 # Bot configuration
-API_ID = YOUR_API_ID
-API_HASH = "YOUR_API_HASH"
-BOT_TOKEN = "YOUR_BOT_TOKEN"
+API_ID = 28724954
+API_HASH = "3ac42791738b153a980e2cdabf97e02f"
+BOT_TOKEN = "8451065628:AAG-WfK58tUG8OFFoUyyqg0CAQw067WXRVM"
 
 # Command prefixes
 COMMAND_PREFIX = [",", ".", "/", "!"]
 
 # Developer details
-DEVELOPER_ID = YOUR_USER_ID  # Replace with the developer's actual user ID
-UPDATES_CHANNEL = "YOUR_CHANNEL_URL"  # URL of the updates channel
+DEVELOPER_ID = 8283377380 # Replace with the developer's actual user ID
+UPDATES_CHANNEL = "https://t.me/mrulpbotchannel"  # URL of the updates channel
 
-ADMIN_IDS = [FIRST_ADMIN_ID, SECOND_ADMIN_ID]
+ADMIN_IDS = [8283377380, 7027577255]
 
 # MongoDB configuration
-MONGO_URL = "YOUR_MONGO_URL"
+MONGO_URL = "mongodb+srv://ioanm866_db_user:CagY25OCDMRBR0Wb@cluster0.ogzuvjb.mongodb.net/"
